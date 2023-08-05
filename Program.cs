071da08189a0f8549e45f9331c87cf796204d514
Guilherme Nascimento de Farias <guiguidefarias@hotmail.com>
2023-08-05 16:48:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+
+const string connectionString = "Server=localhost,1433;Database=balta;User Id=sa;Password=@Teste123@;TrustServerCertificate=True";
+
+using (var connection = new SqlConnection(connectionString))
+{
+    Console.WriteLine("Conectado");
+    connection.Open();  
+    
+    using(var command = new SqlCommand())
+    {
+        command.Connection = connection;
+        command.CommandType = System.Data.CommandType.Text;
+        command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+        var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+        }
+    }
+}
